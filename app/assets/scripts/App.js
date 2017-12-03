8d45c7e8025d0f54941632a85d84ff6a33be4e9c
HonestPhil <phillipjamaal@gmail.com>
2017-12-03 00:07:50 -0500

    initialized the carousel

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,6 @@
 import Intro from './modules/Typewriter';
 import $ from 'jquery';
-
+import MyCarousel from './modules/Carousel';
 
 var heroArea = $('.large-hero__text-content');
 var introTypewriter = new Intro(heroArea,
@@ -11,3 +11,5 @@ var introTypewriter = new Intro(heroArea,
   ],true);
 
 // var scrollButton = new ScrollButton(true);
+
+var myCarousel = new MyCarousel();
